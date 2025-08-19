925b3b5b12cab800a55862f70bf9142ea7d39953
Jesus Ariza Jr <jariza7361@gmail.com>
2025-08-19 05:37:14 -0400

    chore: update Vite build config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,19 @@ export default defineConfig({
   root: './src',
   build: {
     outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        styles: './styles/styles.css',
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'assets/[name]-[hash].css';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
   },
 });
