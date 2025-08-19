fdfde524ac91ffa6445aa64f0aec365bb1de181f
Jesus Ariza Jr <jariza7361@gmail.com>
2025-08-19 05:55:29 -0400

    chore: simplify Vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src',
+  root: 'src',
   build: {
-    outDir: '../dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        styles: './styles/styles.css',
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
-            return 'assets/[name]-[hash].css';
-          }
-          return 'assets/[name]-[hash][extname]';
-        },
-      },
-    },
+    outDir: 'dist',
   },
 });
